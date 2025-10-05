bc9e97b0e7b22e1200321bc9c0b23a7a9ae20f29
HENTAI1909 <113054255+HENTAI1909@users.noreply.github.com>
2025-10-05 17:40:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+double w = 25;
+double y = 13;
+double G = (9.33 * Math.Pow(w, 3) + Math.Sqrt(w)) / (Math.Log(y + 3.5) + Math.Sqrt(y));
+Console.WriteLine($"G = {G}");
+Console.ReadKey();
