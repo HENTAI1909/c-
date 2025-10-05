1760c13c1e5da78552cdb5a478d54fa8c8d7a10f
HENTAI1909 <113054255+HENTAI1909@users.noreply.github.com>
2025-10-05 18:12:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+{
+    Console.Write("введите трехзнач число");
+    int n=int.Parse(Console.ReadLine());
+        int result = (n % 100) * 10 + n / 100;
+        Console.WriteLine(result);
+}
